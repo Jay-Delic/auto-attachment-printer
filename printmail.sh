1933d61b133b229f8e01ed7dfa9a93332a45a591
Noshbit <93923548+Noshbit@users.noreply.github.com>
2024-08-09 04:08:14 +0200

    Update printmail.sh

--- printmail.sh
+++ printmail.sh
@@ -13,22 +13,22 @@
 WAKEUP="03:30" # Wake up at this time tomorrow and run a command
 
 DATE=`date "+%d-%m-%Y %H:%M:%S"`
-echo $DATE " | Booting Auto-Attachment-Process" | tee -a $LOGFILE
+echo $DATE " | " $HOSTNAME " | Booting Auto-Attachment-Process" | tee -a $LOGFILE
 
 # change directory
-echo $DATE " | Switching directory to : $BASEDIR"
+echo $DATE " | " $HOSTNAME " | Switching directory to : $BASEDIR"
 cd $BASEDIR
 # create log file if it does not exist
 touch $LOGFILE
 date +%r-%-d/%-m/%-y >> $LOGFILE
 # fetch mail
-echo $DATE " | Checking for new mail..."
+echo $DATE " | " $HOSTNAME " | Checking for new mail..."
 fetchmail -f $HOMEDIR/fetchmail.conf -L $LOGFILE
 # process new mails
 shopt -s nullglob
 for i in $MAILDIR/new/*
 do
-  echo $DATE " | Processing : $i" | tee -a $LOGFILE
+  echo $DATE " | " $HOSTNAME " | Processing : $i" | tee -a $LOGFILE
   uudeview $i -i -p $ATTACH_DIR/
 # process file attachments with space (thanks to Dr.B.)
    cd $ATTACH_DIR
@@ -42,25 +42,25 @@ do
    done
    cd $BASEDIR
 # end of Dr.B. patch
-  echo $DATE " | Printing PDFs" | tee -a $LOGFILE
+  echo $DATE " | " $HOSTNAME " | Printing PDFs" | tee -a $LOGFILE
   for x in $ATTACH_DIR/*.pdf
   do
-          echo $DATE " | Printing : $x" | tee -a $LOGFILE
+          echo $DATE " | " $HOSTNAME " | Printing : $x" | tee -a $LOGFILE
           lp $x | tee -a $LOGFILE
-          echo $DATE " | Deleting file : $x" | tee -a $LOGFILE
+          echo $DATE " | " $HOSTNAME " | Deleting file : $x" | tee -a $LOGFILE
           rm $x | tee -a $LOGFILE
   done
-  echo $DATE " | Clean up and remove any other attachments"
+  echo $DATE " | " $HOSTNAME " | Clean up and remove any other attachments"
   for y in $ATTACH_DIR/*
   do
           rm $y
   done
   # delete mail
-  echo $DATE " | Deleting mail : $i" | tee -a $LOGFILE
+  echo $DATE " | " $HOSTNAME " | Deleting mail : $i" | tee -a $LOGFILE
   rm $i | tee -a $LOGFILE
 done
 shopt -u nullglob
-echo $DATE " | Job finished at:" $DATE | tee -a $LOGFILE
+echo $DATE " | " $HOSTNAME " | Job finished at:" $DATE | tee -a $LOGFILE
 cd $CURDIR
 
 
@@ -83,27 +83,27 @@ do
     SECS=$(expr `date -d "tomorrow $WAKEUP" +%s` - `date -d "now" +%s`)
     
     echo " " | tee -a $LOGFILE
-    echo $DATE " | Next run will be on" $NEXTRUN "at" $WAKEUP | tee -a $LOGFILE
+    echo $DATE " | " $HOSTNAME " | Next run will be on" $NEXTRUN "at" $WAKEUP | tee -a $LOGFILE
     echo " " | tee -a $LOGFILE
     sleep $SECS & 
     wait $!
-    echo $DATE " | Starting process..." | tee -a $LOGFILE
+    echo $DATE " | " $HOSTNAME " | Starting process..." | tee -a $LOGFILE
     # Run your command here
 
 # change directory
-echo $DATE " | Switching directory to : $BASEDIR"
+echo $DATE " | " $HOSTNAME " | Switching directory to : $BASEDIR"
 cd $BASEDIR
 # create log file if it does not exist
 touch $LOGFILE
 date +%r-%-d/%-m/%-y >> $LOGFILE
 # fetch mail
-echo $DATE " | Checking for new mail..."
+echo $DATE " | " $HOSTNAME " | Checking for new mail..."
 fetchmail -f $HOMEDIR/fetchmail.conf -L $LOGFILE
 # process new mails
 shopt -s nullglob
 for i in $MAILDIR/new/*
 do
-  echo $DATE " | Processing : $i" | tee -a $LOGFILE
+  echo $DATE " | " $HOSTNAME " | Processing : $i" | tee -a $LOGFILE
   uudeview $i -i -p $ATTACH_DIR/
 # process file attachments with space (thanks to Dr.B.)
    cd $ATTACH_DIR
@@ -117,25 +117,25 @@ do
    done
    cd $BASEDIR
 # end of Dr.B. patch
-  echo $DATE " | Printing PDFs" | tee -a $LOGFILE
+  echo $DATE " | " $HOSTNAME " | Printing PDFs" | tee -a $LOGFILE
   for x in $ATTACH_DIR/*.pdf
   do
-          echo $DATE " | Printing : $x" | tee -a $LOGFILE
+          echo $DATE " | " $HOSTNAME " | Printing : $x" | tee -a $LOGFILE
           lp $x | tee -a $LOGFILE
-          echo $DATE " | Deleting file : $x" | tee -a $LOGFILE
+          echo $DATE " | " $HOSTNAME " | Deleting file : $x" | tee -a $LOGFILE
           rm $x | tee -a $LOGFILE
   done
-  echo $DATE " | Clean up and remove any other attachments"
+  echo $DATE " | " $HOSTNAME " | Clean up and remove any other attachments"
   for y in $ATTACH_DIR/*
   do
           rm $y
   done
   # delete mail
-  echo $DATE " | Deleting mail : $i" | tee -a $LOGFILE
+  echo $DATE " | " $HOSTNAME " | Deleting mail : $i" | tee -a $LOGFILE
   rm $i | tee -a $LOGFILE
 done
 shopt -u nullglob
-echo $DATE " | Job finished at:" $DATE | tee -a $LOGFILE
+echo $DATE " | " $HOSTNAME " | Job finished at:" $DATE | tee -a $LOGFILE
 cd $CURDIR
 
 done
